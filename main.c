6aec651b8f2bce5bf6de961e4fa33bbf622f1a8a
saidhann <saidhnn@gmail.com>
2024-04-15 13:31:00 +0200

    First Commit
    Pushing Inicial code conecting to pico and flashing led

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-#include "header.h"
+#include "pico/cyw43_arch.h"
 
 int main() {
     stdio_init_all();
-    while (1) {
-        print_hello_world();
-        sleep_ms(1000);
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
     }
-    return 0;
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+    }
 }
